84b06040ef05cd8d9e05ee2f344f9ce34c6c7b81
C. Bryan Daniels <cdaniels@nandor.net>
2014-06-03 00:48:21 -0500

    Removed clojure stuff and got javascript working

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -39,15 +39,14 @@
 ;; Add in your own as you wish:
 (defvar my-packages '(better-defaults
 		      paredit paredit-everywhere
-		      idle-highlight-mode find-file-in-project smex ido-ubiquitous magit
-		      clojure-mode dash cider auto-complete ac-nrepl
-		      rainbow-delimiters ace-jump-mode projectile
-		      ;; From Balaji S. Srinivasan, Standford
-		      jade-mode sws-mode stylus-mode
-		      cl ffap ansi-color recentf whitespace smooth-scrolling column-marker
-		      ;; From CBD
-		      undo-tree
-		      )
+                      idle-highlight-mode find-file-in-project smex ido-ubiquitous magit
+                      dash auto-complete rainbow-delimiters ace-jump-mode projectile 
+                      ;; From Balaji S. Srinivasan, Standford
+                      jade-mode sws-mode stylus-mode
+                      cl ffap ansi-color recentf whitespace smooth-scrolling column-marker
+                      ;; From CBD
+                      undo-tree
+                      )
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -65,7 +64,6 @@
 
 (ido-mode t)
 (menu-bar-mode -1)
-;(normal-erase-is-backspace-mode 1)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (setq column-number-mode t)
@@ -74,15 +72,121 @@
 (setq show-trailing-whitespace t)
 (setq suggest-key-bindings t)
 (setq vc-follow-symlinks t)
+;(normal-erase-is-backspace-mode 1)
+(global-set-key "\M-?" 'help)
+(global-set-key "\M-\/" 'help-command)
 
-;; Assigns C-h the 'Delete-key' to C-? the 'Bacskspace-key'
-(keyboard-translate ?\C-h ?\C-?)
+;; ------------
+;; -- General Macros --
+;; ------------
+(load "defuns-config.el")
+(fset 'align-equals "\C-[xalign-regex\C-m=\C-m")
+(global-set-key "\M-=" 'align-equals)
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c;" 'comment-or-uncomment-region)
+(global-set-key "\M-n" 'next5)
+(global-set-key "\M-p" 'prev5)
+(global-set-key "\M-o" 'other-window)
+(global-set-key "\M-i" 'back-window)
+(global-set-key "\C-z" 'zap-to-char)
+(global-set-key "\C-h" 'backward-delete-char)
+(global-set-key "\M-d" 'delete-word)
+(global-set-key "\M-h" 'backward-delete-word)
+(global-set-key "\M-u" 'zap-to-char)
 
-;; Assigns 'help' to the 'C-\ key'
-(keyboard-translate ?\C-\\ ?\C-h)
+
+;; ---------------------------
+;; -- JS Mode configuration --
+;; ---------------------------
+(load "js-config.el")
+;;(add-to-list 'load-path "~/.emacs.d/jade-mode") ;; github.com/brianc/jade-mode
+;;(require 'sws-mode)
+;;(require 'jade-mode)
+(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+
+;; ---------------------------
+;; -- Paredit configuration --
+;; ---------------------------
+
+(require 'paredit)
+(define-key paredit-mode-map (kbd "C-M-]") 'paredit-forward-barf-sexp)
+(define-key paredit-mode-map (kbd "C-M-[") 'paredit-backward-barf-sexp)
+(define-key paredit-mode-map (kbd "M-]") 'paredit-forward-slurp-sexp)
+(define-key paredit-mode-map (kbd "M-[") 'paredit-backward-slurp-sexp)
+(require 'paredit-everywhere)
+(add-hook 'prog-mode-hook 'paredit-everywhere-mode)
 
 ;; Always show matching parens
 (show-paren-mode 1)
+
+;; ---------------------------
+;; -- Ace Jump configuration --
+;; ---------------------------
+
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+
+;; -----------------------------------------
+;; -- Rainbow-delimiters-mode configuration --
+;; -----------------------------------------
+
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
+
+;; -----------------------------------------
+;; -- Projectile-global-mode configuration --
+;; -----------------------------------------
+;;(require 'undo-tree)
+
+;; -----------------------------------------
+;; -- Projectile-global-mode configuration --
+;; -----------------------------------------
+
+(projectile-global-mode)
+
+;; -----------------------------------------
+;; -- Clipboard configuration --
+;; -----------------------------------------
+
+(setq x-select-enable-clipboard t)
+(defun yank-to-x-clipboard ()
+  (interactive)
+  (if (region-active-p)
+        (progn
+          (shell-command-on-region (region-beginning) (region-end) "xsel -i -b")
+          (message "Yanked region to clipboard!")
+          (deactivate-mark))
+    (message "No region active; can't yank to clipboard!")))
+
+(define-key global-map (kbd "M-W") 'yank-to-x-clipboard)
+
+
+;; -----------------------------------------
+;; -- Clipboard configuration --
+;; -----------------------------------------
+
+(setq x-select-enable-clipboard t)
+(defun yank-to-x-clipboard ()
+  (interactive)
+  (if (region-active-p)
+        (progn
+          (shell-command-on-region (region-beginning) (region-end) "xsel -i -b")
+          (message "Yanked region to clipboard!")
+          (deactivate-mark))
+    (message "No region active; can't yank to clipboard!")))
+
+(define-key global-map (kbd "M-W") 'yank-to-x-clipboard)
 
 ;; Allow C-y to be used to copy and paste from OS X
 (defun copy-from-osx ()
@@ -95,9 +199,53 @@
 (setq interprogram-cut-function 'paste-to-osx)
 (setq interprogram-paste-function 'copy-from-osx)
 
+
+;; -----------------------------------------
+;; -- Misc configuration --
+;; -----------------------------------------
+
 ;; Change default spell-checking program
 (setq-default ispell-program-name "aspell")
 
 ;; Set my email address
 ;; Setup mail-transport agent to accept smtp traffic, i.e., postfix on os x
 (setq user-mail-address "cdaniels@nandor.net")
+
+
+;; -----------------------------------------
+;; -- Custom Variable configuration --
+;; -----------------------------------------
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((epa-file-encrypt-to arthur@ulfeldt\.com) (whitespace-line-column . 80) (lexical-binding . t)))))
+
+
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(default ((t (:inherit nil :stipple nil :background "color-234" :foreground "unspecified-fg" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
+ '(column-marker-1 ((t (:background "red"))))
+ '(diff-added ((t (:foreground "cyan"))))
+ '(flymake-errline ((((class color) (background light)) (:background "Red"))))
+ '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
+ '(fundamental-mode-default ((t (:inherit default))))
+ '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+ '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
+ '(linum ((t (:foreground "black" :weight bold))))
+ '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+ '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
+ '(show-paren-match ((((class color) (background light)) (:background "black"))))
+ '(vertical-border ((t nil))))
