f9ed447f59de73b9fe5bbda755a4a10790af756f
C. Bryan Daniels <cdaniels@nandor.net>
2014-06-05 02:09:25 -0500

    Configured Smex

--- init.el
+++ init.el
@@ -157,6 +157,17 @@
 (projectile-global-mode)
 
 ;; -----------------------------------------
+;; -- Smex configuration --
+;; -----------------------------------------
+
+(autoload 'smex "smex"
+    "Smex is a M-x enhancement for Emacs, it provides a convenient interface to
+your recently and most frequently used commands.")
+
+(global-set-key (kbd "M-x") 'smex)
+
+
+;; -----------------------------------------
 ;; -- Clipboard configuration --
 ;; -----------------------------------------
 
