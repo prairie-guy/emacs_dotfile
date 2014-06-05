bc9c68249faf39e046f682259b91017a73ac5b64
C. Bryan Daniels <cdaniels@nandor.net>
2014-06-05 01:58:06 -0500

    Remap C-a to 'smarter-move-beginning-of-line'

--- custom.d/defuns-config.el
+++ custom.d/defuns-config.el
@@ -57,3 +57,30 @@ and their terminal equivalents.")
   (or (and window-system (read-kbd-macro desc))
       (or (cdr (assoc desc real-keyboard-keys))
 	  (read-kbd-macro desc))))
+
+(defun smarter-move-beginning-of-line (arg)
+    "Move point back to indentation of beginning of line.                                                                    
+                                                                                                                           
+Move point to the first non-whitespace character on this line.                                                             
+If point is already there, move to the beginning of the line.                                                              
+Effectively toggle between the first non-whitespace character and                                                          
+the beginning of the line.                                                                                                 
+                                                                                                                           
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If                                                              
+point reaches the beginning or end of the buffer, stop there."
+    (interactive "p")
+    (setq arg (or arg 1))
+
+    ;; Move lines first
+    (when (/= arg 1)
+      (let ((line-move-visual nil))
+        (forward-line (1- arg))))
+
+    (let ((orig-point (point)))
+      (back-to-indentation)
+      (when (= orig-point (point))
+        (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line]
+                'smarter-move-beginning-of-line)
