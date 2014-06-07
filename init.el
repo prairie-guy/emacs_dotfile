595019fdbe1db065495a85f3b84992a917a3c2fe
C. Bryan Daniels <cdaniels@nandor.net>
2014-06-07 11:44:11 -0500

    Trying to get load-theme to work. Only certain themes seem to work. tsdh-dar is one of that does.

--- init.el
+++ init.el
@@ -253,12 +253,12 @@ your recently and most frequently used commands.")
 ;; -- Custom Variable configuration --
 ;; -----------------------------------------
 
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(safe-local-variable-values (quote ((epa-file-encrypt-to arthur@ulfeldt\.com) (whitespace-line-column . 80) (lexical-binding . t)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((epa-file-encrypt-to arthur@ulfeldt\.com) (whitespace-line-column . 80) (lexical-binding . t)))))
 
 
 ;; (custom-set-faces
@@ -268,21 +268,21 @@ your recently and most frequently used commands.")
 ;;  ;; If there is more than one, they won't work right.
 ;;  '(default ((t (:inherit nil :stipple nil :background "color-234" :foreground "unspecified-fg" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
 
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
-;;  '(column-marker-1 ((t (:background "red"))))
-;;  '(diff-added ((t (:foreground "cyan"))))
-;;  '(flymake-errline ((((class color) (background light)) (:background "Red"))))
-;;  '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
-;;  '(fundamental-mode-default ((t (:inherit default))))
-;;  '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
-;;  '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
-;;  '(linum ((t (:foreground "black" :weight bold))))
-;;  '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
-;;  '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
-;;  '(show-paren-match ((((class color) (background light)) (:background "black"))))
-;;  '(vertical-border ((t nil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
+ '(column-marker-1 ((t (:background "red"))))
+ '(diff-added ((t (:foreground "cyan"))))
+ '(flymake-errline ((((class color) (background light)) (:background "Red"))))
+ '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
+ '(fundamental-mode-default ((t (:inherit default))))
+ '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+ '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
+ '(linum ((t (:foreground "black" :weight bold))))
+ '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+ '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
+ '(show-paren-match ((((class color) (background light)) (:background "black"))))
+ '(vertical-border ((t nil))))
